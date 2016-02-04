f6534d0c1fa6cf95949676d8a1bf33c0112ba792
Ron Egli <ron@r-egli.com>
2016-02-03 20:40:11 -0700

    Update pythontest.py

--- playground/pythontest.py
+++ playground/pythontest.py
@@ -4,19 +4,19 @@ import urllib2
 import argparse
 import os
 import ssl
-from subprocess import call
+import subprocess
+import socket
 
 arguments = argparse.ArgumentParser()
+arguments.add_argument('--code','-c', required=False, default="200")
+arguments.add_argument('--url','-u', required=False, default='https://cronwtf.com/python/?code=')
 arguments.add_argument('--verbose','-v', help="Prints to screen the activity / response of the script", required=False, action='store_true')
 args = arguments.parse_args()
 verbosity = args.verbose
+incode = args.code
+url = args.url
 
-#if verbosity is True:
-#	print verbosity
-#else:
-#	print "QuietMode"
-
-def sendAlert(server, data):
+def post(server, data):
         req = urllib2.Request(server)
         req.add_header('Content-Type', 'application/json')
 
@@ -25,12 +25,61 @@ def sendAlert(server, data):
         ctx.check_hostname = False
         ctx.verify_mode = ssl.CERT_NONE
 
-        # response = urllib2.urlopen(req, data)
         response = urllib2.urlopen(req, data, context=ctx)
         if verbosity is True:
                 print "[-] Response code: ", response.getcode()
                 print "[-] Returned content:\n", response.read()
-        return response
+        return response.getcode()
+
+def post2(server, data):
+        req = urllib2.Request(server)
+        req.add_header('Content-Type', 'application/json')
+
+        # SSL
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+
+        try:
+                response = urllib2.urlopen(req, data, context=ctx)
+        except urllib2.HTTPError, err:
+                #if err.code == 404:
+                #        print "Page not found!"
+                #elif err.code == 403:
+                #        print "Access denied!"
+                #elif err.code == 500:
+                #        print "Server error!"
+                #else:
+                #        print "Something happened! Error code", err.code 
+                print "[-] Response code: ", err.code              
+                return 0
+        print "[-] Response code: ", response.getcode()
+        print "[-] Returned content:\n", response.read()
+        return 1
+
+def post3(server, data):
+        req = urllib2.Request(server)
+        #req.add_header('Content-Type', 'application/json')
+
+        # SSL
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+
+        output = {}
+        output['url'] = server
+        output['data'] = data
+
+        try:
+                response = urllib2.urlopen(req, data, context=ctx)
+        except urllib2.HTTPError, err:
+                output['code'] = err.code
+                output['response'] = ""           
+                return output
+        else:
+                output['code'] = response.getcode()
+                output['response'] = response.read()
+        return output
 
 
 ###########################################################################
@@ -39,11 +88,30 @@ def sendAlert(server, data):
 #
 ###########################################################################
 
-json = '{"message":"hello world"}'
-server = "https://cronwtf.com/python/"
+if url == "":
+        server = "https://cronwtf.com/python/?code="
+else:
+        server = url
 
-sendAlert(server, json)
+#server = "https://cronwtf.com/python/?code="
+#hostname = socket.gethostname()
+#post(server, json)
 
-#print call(["ls", "-larth"])
-#print call(["ifconfig", "| grep 192"])
-#subprocess.call("ifconfig", shell=True)
+#responseCode = post(server, "")
+
+#if responseCode == 200:
+#        print "YEY"
+#else:
+#        print "BOO"
+
+#myip = subprocess.check_output("ifconfig | grep 192", shell=True)
+#print "My Internal Ip(s): ", myip
+
+output = post3(server, "")
+print output
+
+#if output['code'] == 200:
+#        print "Yey"
+#else:
+#        print "Boo"
+#        print output
