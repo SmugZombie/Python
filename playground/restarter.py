fd309e371993a869c270cbebdcd50673875b5c2a
Ron Egli <ron@r-egli.com>
2016-02-24 17:16:34 -0700

    Create restarter.py

---
+++ playground/restarter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+process_name= "foobot.py" # change this to the name of your process
+
+tmp = os.popen("ps -Af").read()
+
+if process_name not in tmp[:]:
+    print "The process is not running. Let's restart."
+    """"Use nohup to make sure it runs like a daemon"""
+    newprocess="nohup python %s &amp;" % (process_name)
+    os.system(newprocess)  
+else:
+    print "The process is running."
