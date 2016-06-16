0a493b62d163d4c26979857bedfa5870ebb8fc73
Ron Egli <ron@r-egli.com>
2016-06-16 14:15:26 -0700

    0.11 - Cleaned up and commented

--- TinySyslogServer/syslog.py
+++ TinySyslogServer/syslog.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 ## Tiny Python Syslog Server
-## Version 0.10
+## Version 0.11
 
 ## Ron Egli - github.com/smugzombie
-import SocketServer
-import time
-import os
+import SocketServer, time, os
 
-HOST, PORT = "0.0.0.0", 514
-LOG_PATH = '../logs/'
-LOG_FILE = LOG_PATH+'syslog.log'
-OLD_LOG_FILE = LOG_PATH+'syslog.yesterday.log'
+# User Definable Variables
+HOST, PORT = "0.0.0.0", 514           # Listener IP and Port
+LOG_PATH = '../logs/'                 # Path to where you want the logs to go
+LOG_FILE = LOG_PATH+'syslog.log'      # Name of the log file to write to
+OLD_LOG_FILE = LOG_PATH+'syslog.yesterday.log' # Name of the log file to rotate to
+MAX_LOG_SIZE = 50                     # Max log file size before rotation in Megabytes
 
-statcount = 0
+# Dynamic Variables
+statCount = 0                         # Incremental to be used by the script
+sizeCheck = 250                       # Recheck log file size after every X logs written
 
 def checkLogDir(LOG_PATH):
+	# If the log directory doesn't exist, create it.
 	if not os.path.exists(LOG_PATH):
 		os.makedirs(LOG_PATH)
 
@@ -25,13 +28,14 @@ def Logger(message):
 	log.close()               # Close file
 
 def checkRotate():
-	logSize = os.stat(LOG_FILE).st_size
-	megaBytes = logSize / 1048576
-	Logger("Log Size: "+str(logSize)+" bytes = " +str(megaBytes)+" MegaBytes")
-	if megaBytes >= 50:
+	# Check the size of the file, if above MAX_LOG_SIZE fire off logRotate, otherwise keep logging
+	logSize = os.stat(LOG_FILE).st_size # filesize in bytes
+	megaBytes = logSize / 1048576       # convert to megabytes
+	if megaBytes >= MAX_LOG_SIZE:
 		logRotate()
 
 def logRotate():
+	# Delete OLD_LOG_FILE and move LOG_FILE to its place.
 	os.remove(OLD_LOG_FILE)
 	os.rename(LOG_FILE, OLD_LOG_FILE)
 
@@ -46,11 +50,12 @@ class SyslogUDPHandler(SocketServer.BaseRequestHandler):
 		socket = self.request[1]
 		message = str(now)+" "+str(self.client_address[0])+" "+str(data)
 		Logger(message)
-		statcount += 1
-		if statcount >= 25:
+		statCount += 1
+		if statCount >= 250:
 			checkRotate()
 			statcount = 0
 
+# MAIN
 if __name__ == "__main__":
 	try:
 		server = SocketServer.UDPServer((HOST,PORT), SyslogUDPHandler)
