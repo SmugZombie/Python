f21005be8e79e8a2f83186348e48a7ff785df49a
Ron Egli <ron@r-egli.com>
2016-06-16 18:28:44 -0700

    0.13 - Addition of win32 python modules

--- TinySyslogServer/syslog.py
+++ TinySyslogServer/syslog.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 ## Tiny Python Syslog Server
-## Version 0.12.2
+## Version 0.13
 
 ## Ron Egli - github.com/smugzombie
 import SocketServer, time, os, ConfigParser
+import pythoncom
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
 
 # User Definable Variables
-CURRENT_PATH = str(os.path.dirname(os.path.realpath(__file__))) + "/"
+CURRENT_PATH = str(os.path.dirname(os.path.realpath('__file__'))) + "/"
 CONFIG_PATH = CURRENT_PATH+"syslog_server.ini"
 
 # INI Defaults - Changes here will not take effect unless you remove the ini file
@@ -89,8 +95,31 @@ class SyslogUDPHandler(SocketServer.BaseRequestHandler):
 			checkRotate()
 			statcount = 0
 
+class AppServerSvc (win32serviceutil.ServiceFramework):
+    _svc_name_ = "SyslogServer"
+    _svc_display_name_ = "SyslogServer"
+
+    def __init__(self,args):
+        win32serviceutil.ServiceFramework.__init__(self,args)
+        self.hWaitStop = win32event.CreateEvent(None,0,0,None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_,''))
+        self.main()
+
+    def main(self):
+        pass
+
 # MAIN
 if __name__ == "__main__":
+	win32serviceutil.HandleCommandLine(AppServerSvc)
 	try:
 		server = SocketServer.UDPServer((HOST,PORT), SyslogUDPHandler)
 		server.serve_forever(poll_interval=0.5)
